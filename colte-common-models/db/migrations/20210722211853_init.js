66e1a6c5bb95c1d82fe8ec0974405b8d3d264d0a
Javi <infercom2@gmail.com>
2023-05-15 14:56:54 -0600

    Add some fields to the haulage DB structure, to manage customers  and add packages type , topup, sales and transferers history

--- colte-common-models/db/migrations/20210722211853_init.js
+++ colte-common-models/db/migrations/20210722211853_init.js
@@ -6,6 +6,10 @@ exports.up = function (knex) {
       table.string("name", 32).notNullable();
       table.string("symbol", 8).notNullable();
     })
+    .createTable("type_customers", (table) =>{
+      table.increments("id").notNullable().primary();
+      table.string("name").notNullable();
+    })
     .createTable("customers", (table) => {
       table.increments("id").notNullable().primary();
       table.string("imsi", 16).notNullable().unique();
@@ -17,6 +21,8 @@ exports.up = function (knex) {
       table.string("msisdn", 16).notNullable();
       table.bigInteger("pending_data_balance");
       table.uuid("pending_data_balance_txn");
+      table.integer("type").references("type_customers.id").defaultTo(1);
+
     })
     .createTable("actions", (table) => {
       table.increments("id").notNullable().primary();
@@ -32,7 +38,36 @@ exports.up = function (knex) {
       table.boolean("new_msisdn").notNullable();
       table.json("action_payload").notNullable();
       table.primary(["time", "customer"]);
+    })
+    .createTable("packages", (table) => {
+      table.increments("id").notNullable().primary();
+      table.string("name").notNullable();
+      table.string("type_package").notNullable();
+      table.string("type_users").notNullable().references("customers.type").defaultTo(1);
+      table.integer("days_of_use");
+    })
+    .createTable("topup_sales_history", (table) => {
+      table.increments("id").notNullable().primary();
+      table.timestamp("date").notNullable();
+      table.string("imsi", 16).notNullable();
+      table.integer("amount").notNullable();
+    } ) 
+    .createTable("packages_sales_history", (table) => {
+      table.increments("id").notNullable().primary();
+      table.timestamp("date").notNullable();
+      table.string("imsi",16).notNullable();
+      table.integer("cost").notNullable();
+      table.bigInteger("data_mb");
+    })
+    .createTable("transfers_history", (table) => {
+      table.increments("id").notNullable().primary();
+      table.timestamp("date").notNullable();
+      table.string("sender_imsi",16).notNullable();
+      table.string("receiver_imsi",16).notNullable();
+      table.integer("amount").notNullable();
+      table.string("type_transfer",50).notNullable();
     });
+
 };
 
 exports.down = function (knex) {
@@ -40,5 +75,9 @@ exports.down = function (knex) {
     .dropTable("audit_customers")
     .dropTable("customers")
     .dropTable("currencies")
-    .dropTable("actions");
+    .dropTable("actions")
+    .dropTable("packages")
+    .dropTable("topup_sales_history")
+    .dropTable("packages_sales_history")
+    .dropTable("transfers_history");
 };
