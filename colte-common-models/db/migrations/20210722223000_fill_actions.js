3020b78f6a6a3a86817cae52422f6db1e1646499
Javi <infercom2@gmail.com>
2023-05-15 15:05:51 -0600

    Add fill actions to table type_customers MBB (Mobile Broadband) and MiFi

--- colte-common-models/db/migrations/20210722223000_fill_actions.js
+++ colte-common-models/db/migrations/20210722223000_fill_actions.js
@@ -11,10 +11,30 @@ exports.up = function (knex) {
   });
 };
 
+exports.up = function (knex) {
+  return knex.transaction((trx) => {
+    return trx
+      .insert([
+        {name: "MBB"},
+        {name: "MiFi"},
+      ])
+      .into("type_customers");
+  });
+};
+
+
 exports.down = function (knex) {
   return knex.transaction((trx) => {
     return trx("actions")
       .whereIn("action", ["TRANSFER", "PURCHASE", "ADMIN_TOPUP", "ADMIN_TRANSFER"])
+      .del();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.transaction((trx) => {
+    return trx("type_customers")
+      .whereIn("name", ["MBB", "MiFi"])
       .del();
   });
 };
